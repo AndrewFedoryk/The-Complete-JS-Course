c20558d4ae00f89ddf18523c80006292daf51685
Andrew Fedoryk <andrewfedoryk@gmail.com>
2020-09-10 22:54:18 +0300

    Add solution to Coding Challenge 5

--- Coding Challenge 5/script.js
+++ Coding Challenge 5/script.js
@@ -9,24 +9,25 @@ var jhonFamily = {
                 tip = this.bills[i] * (20/100);
                 this.allTips.push(tip);             
                 this.summTipsBills.push(tip+this.bills[i]);
-                console.log(tip + " it's tip from 50$");
+                // console.log(tip + " it's tip from 50$");
             }
             else if (this.bills[i] > 50 && this.bills[i] <= 200) {
                 tip = this.bills[i] * (15/100);
                 this.allTips.push(tip);
                 this.summTipsBills.push(tip+this.bills[i]);
-                console.log(tip + " it's tip from 50$-200$");
+                // console.log(tip + " it's tip from 50$-200$");
             }
             else  if (this.bills[i] > 200) {
                 tip = this.bills[i] * (10/100);
                 this.allTips.push(tip);
                 this.summTipsBills.push(tip+this.bills[i]);
-                console.log(tip + " it's tip from more then 200$");
+                // console.log(tip + " it's tip from more then 200$");
             };
         }
     }
 };
-//jhonFamily.tipCalc();
+jhonFamily.tipCalc();
+console.log(jhonFamily.allTips + " all tips what spend Jhon's family and summ of tips and  bills payed in restrant " + jhonFamily.bills);
 
 var markFamily = {
     allTips: [],
@@ -39,30 +40,36 @@ var markFamily = {
                 tip = this.bills[i] * (20/100);
                 this.allTips.push(tip);             
                 this.summTipsBills.push(tip+this.bills[i]);
-                console.log(tip + " it's tip from bill less then 100$");
+                // console.log(tip + " it's tip from bill less then 100$");
             }
             else if (this.bills[i] > 100 && this.bills[i] <= 300) {
                 tip = this.bills[i] * (10/100);
                 this.allTips.push(tip);
                 this.summTipsBills.push(tip+this.bills[i]);
-                console.log(tip + " it's tip from 100$-300$");
+                // console.log(tip + " it's tip from 100$-300$");
             }
             else  if (this.bills[i] > 300) {
                 tip = this.bills[i] * (25/100);
                 this.allTips.push(tip);
                 this.summTipsBills.push(tip+this.bills[i]);
-                console.log(tip + " it's tip from more then 300$");
+                // console.log(tip + " it's tip from more then 300$");
             };
         }
     }
 };
 markFamily.tipCalc();
-console.log(markFamily.allTips);
+console.log(markFamily.allTips + " all tips what spend Mark's family and summ of tips and  bills payed in restrant " + markFamily.bills);
+
 function averageTipCalc(tips){
-    var average = 0;
+    var summ = 0;
+    var averageSumm
     for (var i = 0; i < tips.length; i++) {
-        average = average + i;
+        summ = summ + tips[i];
     }
-    console.log(average);
+    return averageSumm = summ / tips.length;
+    
 }
-averageTipCalc(markFamily.allTips);
+// if (averageTipCalc(markFamily.allTips) > averageTipCalc(jhonFamily.allTips)) {
+//     console.log;
+// }
+averageTipCalc(markFamily.allTips) > averageTipCalc(jhonFamily.allTips) ? console.log("Mark family have highest average tips " + averageTipCalc(markFamily.allTips) + " then jhon " + averageTipCalc(jhonFamily.allTips)) : console.log("Jhon's family have highest average tips " + averageTipCalc(jhonFamily.allTips) + " then Mark " + averageTipCalc(markFamily.allTips));
